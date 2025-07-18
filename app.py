295d4e4b2841ab195467efc8ff4ed602228e11e6
hagagfawzi <hhaggag009@gmail.com>
2025-07-18 14:53:38 +0300

    Initial commit - phishing URL detector

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
+import os
+import requests
 from flask import Flask, render_template, request
 import joblib
 import re
 
 app = Flask(__name__)
 
+# ID الخاص بملف Google Drive
+VECTOR_FILE_ID = "1vYmOBtA0U6xMr-h0VG4jWhENgolex8wL"
+VECTOR_FILE_PATH = "tfidf_vectorizer_lr.pkl"
+
+# دالة لتحميل الملف من Google Drive
+def download_vectorizer():
+    if not os.path.exists(VECTOR_FILE_PATH):
+        print("📥 جاري تحميل tfidf_vectorizer_lr.pkl من Google Drive...")
+        url = f"https://drive.google.com/uc?export=download&id={VECTOR_FILE_ID}"
+        response = requests.get(url)
+        with open(VECTOR_FILE_PATH, "wb") as f:
+            f.write(response.content)
+        print("✅ تم التحميل بنجاح.")
+
+# تحميل الملف إذا لم يكن موجودًا
+download_vectorizer()
+
 # تحميل الموديل والـ TF-IDF
 model = joblib.load("logistic_regression_phishing_model.pkl")
-vectorizer = joblib.load("tfidf_vectorizer_lr.pkl")
+vectorizer = joblib.load(VECTOR_FILE_PATH)
 
 # دالة تنظيف الرابط
 def clean_url(url):
@@ -38,5 +57,4 @@ def home():
     return render_template("index.html", result=result, probability=probability)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
